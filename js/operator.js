f71c06d14d89255b0b3d7daa63823b18cdb02401
windowdong11 <windowdong11@naver.com>
2021-04-23 23:29:22 +0900

    feat: Add basic operator

---
+++ js/operator.js
@@ -0,0 +1,34 @@
+// numeric, 산술
+console.log(1 + 2)
+console.log(1 - 2)
+console.log(1 * 2)
+console.log(1 / 2)
+console.log(1 % 2)
+
+// assignment, 대입
+let a = 1
+console.log(a)
+a = 2
+console.log(a)
+a += 1
+console.log(a)
+a -= 1
+console.log(a)
+// ... etc
+
+// comparision operator
+// 타입 자동 변환하여 비교
+console.log("1" == 1.0)
+console.log("1" != 1.0)
+// 타입 변환하지 않고 비교
+console.log("1" === 1.0)
+console.log("1" !== 1.0)
+console.log(1 > 2)
+console.log(1 < 2)
+console.log(1 >= 2)
+console.log(1 <= 2)
+
+// logical, 논리
+console.log(true && false)
+console.log(true || false)
+console.log(!true)
