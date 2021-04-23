2b2dcb4bf2c066779cdfbd60885e5cba76bc073d
windowdong11 <windowdong11@naver.com>
2021-04-23 23:41:23 +0900

    feat: Add condition

---
+++ js/condition.js
@@ -0,0 +1,23 @@
+const a = Math.floor(Math.random() * 10)
+
+if(a == 1){
+    console.log("D.va")
+}
+else if(a == 2){
+    console.log("Yee")
+}
+else {
+    console.log("L's")
+}
+
+switch(a){
+    case 3:
+        console.log("Sam")
+        break
+    case 9:
+        console.log("Pigeon")
+        break
+    default:
+        console.log("D-Fault")
+}
+console.log(a)
