11a30dac6ffbf4c1a63e47146fe5405db97fdfaf
windowdong11 <windowdong11@naver.com>
2021-04-23 23:10:56 +0900

    refactor: Export getType function to module

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import getType from './getType'
 
 console.log("test")
 console.log(_.camelCase('test'))
@@ -6,7 +7,4 @@ console.log(_.camelCase('test'))
 // typeof는 완전한 타입을 나타내지 못함
 // 따라서 getType라는 함수 사용
 console.log(typeof [])
-function getType(data){
-    return Object.prototype.toString.call(data).slice(8, -1)
-}
 console.log(getType([]))
