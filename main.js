63a443a155a1bd9531cd7b95c31f46d034ec66ac
windowdong11 <windowdong11@naver.com>
2021-04-23 23:00:19 +0900

    feat: Add getType()

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ import _ from 'lodash'
 
 console.log("test")
 console.log(_.camelCase('test'))
+
+// typeof는 완전한 타입을 나타내지 못함
+// 따라서 getType라는 함수 사용
+console.log(typeof [])
+function getType(data){
+    return Object.prototype.toString.call(data).slice(8, -1)
+}
+console.log(getType([]))
