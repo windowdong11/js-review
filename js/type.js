f579e6d060b10fcbb6a840d2556e6e87be22ce2f
windowdong11 <windowdong11@naver.com>
2021-04-23 23:54:59 +0900

    feat: Add type

---
+++ js/type.js
@@ -0,0 +1,11 @@
+// true : {}, [], (number)0, -0, NaN을 제외한 모든 수, (string)문자가 하나라도 포함된 문자열
+// false : '', null, undefined, 0, -0, NaN
+
+if({}){
+    console.log("{} : true")
+}
+if(''){
+}
+else {
+    console.log("'': false")
+}
